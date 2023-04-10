a7058c2124226868b10c32c22aa4d59be1f7de19
manu601 <sang601ke@gmail.com>
2023-04-10 13:55:10 -0700

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,52 @@
-#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * read_textfile - Reads a text file and prints it to standard output.
+ * @filename: The name of the file to be read.
+ * @letters: The number of letters to read and print.
  *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ * Return: The actual number of letters read and printed. Returns 0 if the
+ * file cannot be opened or read, if filename is NULL, or if write fails or
+ * does not write the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+    if (filename == NULL)
+        return 0; // Return 0 if filename is NULL
 
-	if (filename == NULL)
-		return (0);
+    int file = open(filename, O_RDONLY); // Open the file in read-only mode
+    if (file == -1)
+        return 0; // Return 0 if the file cannot be opened
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
+    char *buffer = malloc(letters); // Allocate memory for the buffer
+    if (buffer == NULL)
+    {
+        close(file);
+        return 0; // Return 0 if memory allocation fails
+    }
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+    ssize_t bytesRead = read(file, buffer, letters); // Read from file to buffer
+    if (bytesRead == -1)
+    {
+        close(file);
+        free(buffer);
+        return 0; // Return 0 if reading fails
+    }
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
+    ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead); // Write buffer to standard output
+    if (bytesWritten != bytesRead)
+    {
+        close(file);
+        free(buffer);
+        return 0; // Return 0 if writing fails or does not write expected bytes
+    }
 
-	free(buffer);
-	close(o);
+    close(file);
+    free(buffer);
 
-	return (w);
+    return bytesRead; // Return the actual number of letters read and printed
 }
+
